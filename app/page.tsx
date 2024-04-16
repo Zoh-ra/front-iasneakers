85fc528f39242a0ee3f126400c0081bf9ac2fe63
zoh-ra <raitonoums@gmail.com>
2024-04-16 10:21:05 +0200

    update admins

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="relative flex min-h-screen flex-col items-center justify-center">
       <Toaster />
       <div className="flex flex-col gap-4">
-      <Link href="/adminsccrap" 
+      <Link href="/adminscrap" 
         className="group rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
       >
         <p>ADMIN SCRAP</p>
